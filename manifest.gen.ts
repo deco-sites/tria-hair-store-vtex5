7b71bc51012f2b81800bd70bd40cb20078bd141b
Karla Oshikawa <karla_hara@hotmail.com>
2024-06-21 16:23:26 +0000

    Release staging environment with 2 changes made by Karla Oshikawa

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -3,8 +3,9 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $$$$$$$$$$$0 from "./apps/deco/blog.ts";
-import * as $$$$$$$$$$$1 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$2 from "./apps/site.ts";
+import * as $$$$$$$$$$$1 from "./apps/deco/vnda.ts";
+import * as $$$$$$$$$$$2 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$3 from "./apps/site.ts";
 import * as $$$2 from "./loaders/availableIcons.ts";
 import * as $$$3 from "./loaders/icons.ts";
 import * as $$$0 from "./loaders/Layouts/ProductCard.tsx";
@@ -125,8 +126,9 @@ const manifest = {
   },
   "apps": {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,
-    "site/apps/decohub.ts": $$$$$$$$$$$1,
-    "site/apps/site.ts": $$$$$$$$$$$2,
+    "site/apps/deco/vnda.ts": $$$$$$$$$$$1,
+    "site/apps/decohub.ts": $$$$$$$$$$$2,
+    "site/apps/site.ts": $$$$$$$$$$$3,
   },
   "name": "site",
   "baseUrl": import.meta.url,
